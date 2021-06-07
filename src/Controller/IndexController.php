58825c5a3de5433b43d2b7946f50bfe7ba77fe23
shadersy <lobeeva96>
2021-06-07 17:14:18 +0300

    travis #3

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+
 use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class IndexController extends AbstractController
