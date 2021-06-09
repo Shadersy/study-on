a357244223a902efc20286b9a99fe8fe1bd72a88
shadersy <lobeeva96>
2021-06-09 16:13:34 +0300

    Добавлены ограничения на формы

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -5,21 +5,37 @@ namespace App\Form;
 use App\Entity\Course;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class CourseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code')
+            ->add(
+                'code',
+                TextType::class,
+                ['required' =>
+                    true,
+                    'constraints' => [new Length(['max' => 30, 'min' => 3])],
+                    ]
+            )
             ->add('type', ChoiceType::class, ['choices'  => [
             'Бесплатный' => 0,
             'Покупка' => 1,
                 'Аренда' => 2,
         ]])
-            ->add('name')
+            ->add(
+                'name',
+                TextType::class,
+                ['required' =>
+                    true,
+                    'constraints' => [new Length(['max' => 50, 'min' => 3])],
+                ]
+            )
             ->add('description')
             ->add('cost');
     }
